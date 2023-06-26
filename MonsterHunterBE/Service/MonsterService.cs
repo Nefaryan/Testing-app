c4cbd935a2b2a19b8f56aea510fbe4e0e1f9a8fe
Giuseppe Roberti <giuseppe.roberti9950@gmail.com>
2023-06-26 10:51:49 +0200

    All service documented

--- MonsterHunterBE/Service/MonsterService.cs
+++ MonsterHunterBE/Service/MonsterService.cs
@@ -14,54 +14,93 @@ namespace MonsterHunterBE.Service
 
         }
 
+        /// <summary>
+        /// Metodo per recuperare la lsita dei msotri presenti nel db
+        /// </summary>
+        /// <returns></returns>
         public List<Monster> GetAll()
         {
             return repository.GetAll();
         }
 
+        /// <summary>
+        /// Metodo per recuperare un mostro tramite il suo id
+        /// </summary>
+        /// <param name="id">l'id del mostro da recuperare</param>
+        /// <returns>il mostro corrispondente</returns>
         public Monster GetById(Guid id)
         {
             return repository.GetMonsterById(id);
         }
-
+        /// <summary>
+        /// Metodo per l'aggiunta di una nuova entità mostro nel db
+        /// </summary>
+        /// <param name="monster">il mostro da aggiundre</param>
+        /// <returns>il mostro aggiunto</returns>
         public Monster AddMonster(Monster monster)
         {
 
             return repository.AddNewMonster(monster);
         }
-
+        /// <summary>
+        /// Metodo per l'aggioranameto del mostro tramite il suo id
+        /// </summary>
+        /// <param name="id">L'id da aggiornare</param>
+        /// <param name="monster">le modifiche da apportare</param>
+        /// <returns> Il mostro mosdificato</returns>
         public Monster UpdateMonster(Guid id, Monster monster)
         {
             return repository.UpdateMonster(id, monster);
         }
-
+        /// <summary>
+        /// Metodo per eliminare un mostro dal db
+        /// </summary>
+        /// <param name="id">l'id del mostro da eliminare</param>
         public void RemoveMonster(Guid id)
         {
             repository.DeleteMonster(id);
         }
-
-        public void AddDrop(Guid Id, String name)
+        /// <summary>
+        /// Metdo di aggiunta di un drop specifico al mostro
+        /// </summary>
+        /// <param name="Id">L'id del mostro a cui aggiungere il droo</param>
+        /// <param name="name">il nome del drop da aggiungere</param>
+        public void AddDrop(Guid MonsterId, String DropName)
         {
-            repository.AddDropToMonster(Id, name);
+            repository.AddDropToMonster(MonsterId, DropName);
         }
-
+        /// <summary>
+        /// Metodo per l'aggiunta della weakness al mostro
+        /// </summary>
+        /// <param name="MonsterId">IdL'id del mostro</param>
+        /// <param name="WeaknessId">L'id della debolezza</param>
         public void AddWeakness(Guid MonsterId, Guid WeaknessId)
         {
             repository.AddWeaknessToMonster(MonsterId, WeaknessId);
         }
 
+        /// <summary>
+        /// Metodo per la ricerca di tutti i mostri di uno stesso tipo
+        /// </summary>
+        /// <param name="type">il tipo da cercare</param>
+        /// <returns>Tutti i mostri che corrispondono a quel tipo</returns>
         public IEnumerable<Monster> FindAllMonsterOfType(string type)
         {
             List<Monster> monsters = GetAll();
             return monsters.Where(monster => monster.Type.Equals(type));
         }
-
+        /// <summary>
+        /// Metodo per trovare tutti i drop di un mostro tramite il suo nome
+        /// </summary>
+        /// <param name="name">il nome del mostro da cui cercare id rop</param>
+        /// <returns>La lista dei drop del mostro</returns>
         public IEnumerable<MonsterDrop> FindAllDropOfMonster(string name)
         {
 
             Monster monster = repository.FindByName(name);
             if (monster == null)
             {
+                //Se il mostro è nulla viene restituita una lista vuota
                 return Enumerable.Empty<MonsterDrop>();
             }
             List<MonsterDrop> monstersDop = monster.Drop.ToList();
--- MonsterHunterBE/Service/MonsterWeaknessService.cs
+++ MonsterHunterBE/Service/MonsterWeaknessService.cs
@@ -11,27 +11,46 @@ namespace MonsterHunterBE.Service
         {
             _repository = repository;
         }
-
+        /// <summary>
+        /// Metodo per trovare tutte le debolezze presenti nel db
+        /// </summary>
+        /// <returns></returns>
         public List<MonsterWeakness> GetAll()
         {
             return _repository.GetAllWeakness();
         }
-
+        /// <summary>
+        /// Metodo per cercare la singola weakness tramite id
+        /// </summary>
+        /// <param name="id">L'id della weakness da trovare</param>
+        /// <returns>la weakness associata all'id</returns>
         public MonsterWeakness GetById(Guid id)
         {
             return _repository.GetById(id);
         }
-
+        /// <summary>
+        /// Metodo per l'aggiunta di una nuova wekness
+        /// </summary>
+        /// <param name="monsterWeakness">la weakness da aggiungere</param>
+        /// <returns>la weakness aggiunta al db</returns>
         public MonsterWeakness AddWeakness(MonsterWeakness monsterWeakness)
         {
             return _repository.NewWeakness(monsterWeakness);
         }
-
+        /// <summary>
+        /// Metodo per l'update di una weakeness specifica
+        /// </summary>
+        /// <param name="id">L'id della weakness da modificare</param>
+        /// <param name="monsterWeakness">le modifiche da apportare</param>
+        /// <returns>la weakness modificata</returns>
         public MonsterWeakness UpdateWeakness(Guid id, MonsterWeakness monsterWeakness)
         {
             return _repository.UpdateWeak(id, monsterWeakness);
         }
-
+        /// <summary>
+        /// Metodo per l'eliminazione di una weakness specifica
+        /// </summary>
+        /// <param name="id"></param>
         public void DeleteWeakness(Guid id)
         {
             _repository.Delete(id);
