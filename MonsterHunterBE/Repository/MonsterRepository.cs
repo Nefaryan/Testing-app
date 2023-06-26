14067d99c32bc3002b75d1bbc6eecb47fd40a40a
Giuseppe Roberti <giuseppe.roberti9950@gmail.com>
2023-06-26 10:14:13 +0200

    Documetazione

--- MonsterHunterBE/Repository/MonsterRepository.cs
+++ MonsterHunterBE/Repository/MonsterRepository.cs
@@ -17,7 +17,11 @@ namespace MonsterHunterBE.Repository
             this.weaknessRepository = weaknessRepository;
             this.dropRepository = dropRepository;
         }
-
+        /// <summary>
+        /// Metodo per l'aggiunta di un nuovo mostro nel db
+        /// </summary>
+        /// <param name="monster">L'oggetto monster da aggiungere</param>
+        /// <returns>il mostro aggiunto</returns>
         public Monster AddNewMonster(Monster monster)
         {
             monster.Id = Guid.NewGuid();
@@ -25,6 +29,13 @@ namespace MonsterHunterBE.Repository
             hunterContex.SaveChanges();
             return monster;
         }
+        /// <summary>
+        /// Metodo per la ricerca di un mostro tramite il suo id
+        /// </summary>
+        /// <param name="id">L'id del mostro da cercare</param>
+        /// <returns>il mostro</returns>
+        /// <exception cref="Exception">
+        /// messaggio di errore che viene visuallizato se l'id inserito non viene trovato nel db</exception>
 
         public Monster GetMonsterById(Guid id)
         {
@@ -32,6 +43,10 @@ namespace MonsterHunterBE.Repository
             return monster;
         }
 
+        /// <summary>
+        /// Metodo per Recuperare tutti i mostri,drop e weakness a cui sono legati
+        /// </summary>
+        /// <returns></returns>
         public List<Monster> GetAll()
         {
             return hunterContex.Monsters
@@ -39,8 +54,13 @@ namespace MonsterHunterBE.Repository
                 .Include(y => y.Weakness).ToList();
         }
 
-
-
+        /// <summary>
+        /// Metodo di aggiornameto del mostro tramite il suo id, è possibile aggiornare solo
+        /// descrizione, note e imageUrl
+        /// </summary>
+        /// <param name="id">L'id del mostro da aggioranre</param>
+        /// <param name="monster">L'oggetto con gli aggiornameti</param>
+        /// <returns>Il mostro aggiornato</returns>
         public Monster UpdateMonster(Guid id, Monster monster)
         {
             try
@@ -60,6 +80,10 @@ namespace MonsterHunterBE.Repository
             return monster;
         }
 
+        /// <summary>
+        /// Eliminazione del mostro tramite il suo id
+        /// </summary>
+        /// <param name="id">L'id del mostro da eliminare</param>
         public void DeleteMonster(Guid id)
         {
             var monster = hunterContex.Monsters.FirstOrDefault(m => m.Id == id);
@@ -70,6 +94,11 @@ namespace MonsterHunterBE.Repository
             }
         }
 
+        /// <summary>
+        /// Metodo per aggiungere un drop al mostro
+        /// </summary>
+        /// <param name="monsterId">L'id del mostro a cui a aggiungere il dorp</param>
+        /// <param name="dropName">Il nome del drop da aggingere</param>
         public void AddDropToMonster(Guid monsterId, string dropName)
         {
             var monster = GetMonsterById(monsterId);
@@ -83,7 +112,12 @@ namespace MonsterHunterBE.Repository
                 UpdateMonster(monsterId, monster);
             }
         }
-
+        /// <summary>
+        /// Metodo per aggiungere una debolezza al mostro tramite l'id del mostro e l'id della
+        /// debolezza specifica da aggiornare
+        /// </summary>
+        /// <param name="monsterId">L'id del mostro a cui aggiungere la nuova weakness</param>
+        /// <param name="weaknessId">l'id della wekness da aggiungere</param>
         public void AddWeaknessToMonster(Guid monsterId, Guid weaknessId)
         {
             var monster = GetMonsterById(monsterId);
@@ -91,6 +125,7 @@ namespace MonsterHunterBE.Repository
 
             if (monster != null && weakness != null)
             {
+                // Se la lista è nulla viene istanziata
                 monster.Weakness ??= new List<MonsterWeakness>();
 
                 monster.Weakness.Add(weakness);
@@ -98,6 +133,11 @@ namespace MonsterHunterBE.Repository
             }
         }
 
+        /// <summary>
+        /// Metodo per cercare un mostro specifico tramite 
+        /// </summary>
+        /// <param name="name">Il nome del mostro da cercare</param>
+        /// <returns>Il mostro col nome corrispodente</returns>
         public Monster FindByName(string name)
         {
             Monster monster = hunterContex.Monsters.FirstOrDefault(x => x.Name == name);
--- MonsterHunterBE/Repository/MonsterWeaknessRepository.cs
+++ MonsterHunterBE/Repository/MonsterWeaknessRepository.cs
@@ -12,6 +12,13 @@ namespace MonsterHunterBE.Repository
             this.Monstercontext = monstercontext;
         }
 
+        /// <summary>
+        /// Metodo per la creazione di un ogetto MonsterWeak, 
+        /// il metodo assegna un id generato automaticamente all'ogetto e dopo
+        /// lo salva nel db
+        /// </summary>
+        /// <param name="weakness">Body in Json della richiesta</param>
+        /// <returns> L'entità aggiunta al db</returns>
         public MonsterWeakness NewWeakness(MonsterWeakness weakness)
         {
             weakness.Id = Guid.NewGuid();
@@ -20,16 +27,37 @@ namespace MonsterHunterBE.Repository
             return weakness;
         }
 
+
+        /// <summary>
+        /// Metodo per la ricerca dell'oggetto Monsterweakness tramite id,
+        /// il metodo cerca nel db se l'id esiste se non esiste ritorna un eccezione se inve esiste
+        /// ritorna l'oggetto legto all'id
+        /// </summary>
+        /// <param name="id">L'ID da controllare </param>
+        /// <returns>l'oggetto legato all'id</returns>
+        /// <exception cref="Exception">Restituisce un messaggio("Id non found") in caso quell'id non venga trovato
+        /// </exception>
         public MonsterWeakness GetById(Guid id)
         {
-            var weak = Monstercontext.MonsterWeaknesses.FirstOrDefault(w => w.Id == id) ?? throw new Exception("Not found");
+            var weak = Monstercontext.MonsterWeaknesses.FirstOrDefault(w => w.Id == id) ?? throw new Exception(" Id Not found");
             return weak;
         }
-
+        /// <summary>
+        /// Metodo per la ricerca di tutti gli oggetti persenti nel db
+        /// </summary>
+        /// <returns>
+        /// La lista degli ogetti presenti nel db
+        /// </returns>
         public List<MonsterWeakness> GetAllWeakness()
         {
             return Monstercontext.MonsterWeaknesses.ToList();
         }
+        /// <summary>
+        /// Metodo per l'aggiornamento dell'entità Monster weakness, tramite l'identificativo id
+        /// </summary>
+        /// <param name="id">L'id dell oggetto da aggiornre</param>
+        /// <param name="weakness">Il body con le modifiche presenti</param>
+        /// <returns></returns>
         public MonsterWeakness UpdateWeak(Guid id, MonsterWeakness weakness)
         {
             try
@@ -37,6 +65,7 @@ namespace MonsterHunterBE.Repository
                 var upWeak = Monstercontext.MonsterWeaknesses.FirstOrDefault(w => w.Id == id) ?? throw new Exception($"Unable to update drop: {id}");
                 upWeak.WeaknessPerc = weakness.WeaknessPerc;
                 upWeak.Name = weakness.Name;
+                upWeak.Type = weakness.Type;
 
                 Monstercontext.SaveChanges();
             }
@@ -46,7 +75,10 @@ namespace MonsterHunterBE.Repository
             }
             return weakness;
         }
-
+        /// <summary>
+        /// Metodo per l'eliminazione dell'entità tramite id
+        /// </summary>
+        /// <param name="id">L'id da eliminare</param>
         public void Delete(Guid id)
         {
             var weak = Monstercontext.MonsterWeaknesses.FirstOrDefault(w => w.Id == id);
